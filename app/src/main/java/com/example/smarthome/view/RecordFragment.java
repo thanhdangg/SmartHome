fe1532415e52ce8487ff484085fb38bc5368ba19
thanhdangg <102210310@sv1.dut.udn.vn>
2024-06-15 00:55:42 +0700

    Update record with Google

--- app/src/main/java/com/example/smarthome/view/RecordFragment.java
+++ app/src/main/java/com/example/smarthome/view/RecordFragment.java
@@ -1,5 +1,8 @@
 package com.example.smarthome.view;
 
+import static android.app.Activity.RESULT_OK;
+
+import android.content.ActivityNotFoundException;
 import android.content.pm.PackageManager;
 
 import android.content.res.ColorStateList;
@@ -11,10 +14,12 @@ import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Locale;
 
 import android.util.Base64;
@@ -28,8 +33,11 @@ import android.view.ViewGroup;
 import android.os.Handler;
 
 import android.Manifest;
+import android.widget.Toast;
+
 import com.example.smarthome.R;
 import com.example.smarthome.databinding.FragmentRecordBinding;
+import com.example.smarthome.viewmodel.WebSocketManager;
 
 import org.json.JSONObject;
 
@@ -39,6 +47,7 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 
 
+import dev.gustavoavila.websocketclient.WebSocketClient;
 import okhttp3.*;
 import okio.ByteString;
 import okio.Okio;
@@ -48,17 +57,12 @@ import okio.Source;
 public class RecordFragment extends Fragment {
 
     private FragmentRecordBinding binding;
-    private MediaRecorder recorder;
-    private String fileName = null;
-    private static final int REQUEST_CODE_SPEECH_INPUT = 1;
     private static final int REQUEST_RECORD_AUDIO_PERMISSION = 200;
-    private static final int RESULT_OK = 1;
-    private boolean isRecording = false;
-    private final OkHttpClient httpClient = new OkHttpClient();
+    private static final int SPEECH_REQUEST_CODE = 0;
 
-    private MediaPlayer player = null;
-
-
+    private boolean permissionToRecordAccepted = false;
+    private String[] permissions = {Manifest.permission.RECORD_AUDIO};
+    private WebSocketClient mWebSocketClient;
 
 
 
@@ -75,177 +79,91 @@ public class RecordFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (ContextCompat.checkSelfPermission(getContext(), Manifest.permission.RECORD_AUDIO) != PackageManager.PERMISSION_GRANTED) {
-            requestPermissions(new String[]{Manifest.permission.RECORD_AUDIO}, REQUEST_RECORD_AUDIO_PERMISSION);
-        }
-
+        ActivityCompat.requestPermissions(getActivity(), permissions, REQUEST_RECORD_AUDIO_PERMISSION);
+        mWebSocketClient = WebSocketManager.getInstance().getWebSocketClient();
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         binding = FragmentRecordBinding.inflate(inflater, container, false);
-
-        binding.btnRecord.setOnLongClickListener(new View.OnLongClickListener() {
+        binding.btnRecord.setOnClickListener(new View.OnClickListener() {
             @Override
-            public boolean onLongClick(View v) {
-                startRecording();
+            public void onClick(View v) {
+                startVoiceRecognition();
                 binding.txtListening.setText("Đang nghe. . .");
                 binding.btnRecord.setBackgroundTintList(ColorStateList.valueOf(Color.parseColor("#FF0000")));
                 binding.imgRecord.setVisibility(View.VISIBLE);
                 binding.txtTryAgain.setVisibility(View.INVISIBLE);
-                return true; // return true to indicate that the event is consumed
-            }
-        });
-
-        binding.btnRecord.setOnTouchListener(new View.OnTouchListener() {
-            @Override
-            public boolean onTouch(View v, MotionEvent event) {
-                if (event.getAction() == MotionEvent.ACTION_UP) {
-                    stopRecording();
-                    binding.txtListening.setText("Thử lại");
-                    binding.btnRecord.setBackgroundTintList(ColorStateList.valueOf(Color.parseColor("#FF0000")));
-                    binding.imgRecord.setVisibility(View.INVISIBLE);
-                    binding.txtTryAgain.setVisibility(View.VISIBLE);
-                }
-                return false; // return false to let the event propagate to the long click listener
-            }
-        });
-        binding.btnRecord.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
             }
         });
 
         return binding.getRoot();
+    }
+    private void startVoiceRecognition() {
+        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        intent.putExtra(RecognizerIntent.EXTRA_PROMPT, "Speak now...");
+
+        try {
+            startActivityForResult(intent, SPEECH_REQUEST_CODE);
+        } catch (ActivityNotFoundException a) {
+            Toast.makeText(getActivity(), "Your device doesn't support Speech Recognition", Toast.LENGTH_SHORT).show();
+        }
     }
 
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
 
-        if (requestCode == REQUEST_CODE_SPEECH_INPUT && resultCode == RESULT_OK) {
-            ArrayList<String> result = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
-            binding.txtResult.setText(result.get(0));
+        if (requestCode == SPEECH_REQUEST_CODE && resultCode == RESULT_OK) {
+            List<String> results = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+            String spokenText = results.get(0);
+            binding.txtResult.setText(spokenText);
             binding.txtResult.setVisibility(View.VISIBLE);
-        }
-    }
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+            binding.txtListening.setVisibility(View.INVISIBLE);
+            binding.imgRecord.setVisibility(View.INVISIBLE);
 
-        if (requestCode == REQUEST_RECORD_AUDIO_PERMISSION) {
-            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                // Permission granted
+            Log.d("WebSocket", "Result: " + spokenText);
+            String command = "";
+            if (spokenText.contains("bật") || spokenText.contains("mở")) {
+                command = command.concat("B");
+            } else if (spokenText.contains("tắt")) {
+                command = command.concat("B");
             } else {
-                // Permission denied
+                binding.txtTryAgain.setVisibility(View.VISIBLE);
             }
-        }
-    }
-
-    @Override
-    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
-    }
-
-    private void startRecording() {
-        try {
-            recorder = new MediaRecorder();
-            recorder.setAudioSource(MediaRecorder.AudioSource.MIC);
-            recorder.setOutputFormat(MediaRecorder.OutputFormat.MPEG_4);
-            fileName = getActivity().getExternalCacheDir().getAbsolutePath();
-            fileName += "/audiorecordtest.wav";
-            recorder.setOutputFile(fileName);
-            recorder.setAudioEncoder(MediaRecorder.AudioEncoder.AAC);
-            recorder.prepare();
-            recorder.start();
-            isRecording = true;
-        } catch (IOException e) {
-            Log.e("Recording", "prepare() failed", e);
-        }
-    }
-
-    private void stopRecording() {
-        if (isRecording && recorder != null) {
-            try {
-                recorder.stop();
-            } catch (RuntimeException e) {
-                Log.e("Recording", "stop() failed", e);
-            }
-            recorder.release();
-            recorder = null;
-            isRecording = false;
-        }
-        if (fileName != null) {
-            Handler handler = new Handler();
-            handler.postDelayed(new Runnable() {
-                @Override
-                public void run() {
-                    player = new MediaPlayer();
-                    try {
-                        player.setDataSource(fileName);
-                        player.prepare();
-                        player.start();
-                    } catch (IOException e) {
-                        Log.e("Playback", "prepare() failed", e);
-                    }
-                }
-            }, 1000);
-
-            File file = new File(fileName);
-            if (file.exists()) {
-                try {
-                    RequestBody requestBody = RequestBody.create(MediaType.parse("audio/wav"), file);
-                    MultipartBody.Part filePart = MultipartBody.Part.createFormData("file", file.getName(), requestBody);
-                    MultipartBody multipartBody = new MultipartBody.Builder()
-                            .setType(MultipartBody.FORM)
-                            .addPart(filePart)
-                            .build();
-                    Request request = new Request.Builder()
-                            .url("http://3.25.190.4:1911/wss")
-                            .post(multipartBody)
-                            .build();
-                    httpClient.newCall(request).enqueue(new Callback() {
-                        @Override
-                        public void onFailure(Call call, IOException e) {
-                            Log.e("Recording", "POST request failed", e);
-                        }
-                        @Override
-                        public void onResponse(Call call, Response response) throws IOException {
-                            if (response.isSuccessful()) {
-                                final String myResponse = response.body().string();
-                                getActivity().runOnUiThread(new Runnable() {
-                                    @Override
-                                    public void run() {
-                                        binding.txtResult.setText(myResponse);
-                                        binding.txtResult.setVisibility(View.VISIBLE);
-                                    }
-                                });
-                            }
-                        }
-                    });
-                } catch (Exception e) {
-                    Log.e("Recording", "Failed to encode audio file", e);
-                }
-
-//                if (file.delete()) {
-//                    Log.d("Recording", "Audio file deleted successfully");
-//                } else {
-//                    Log.e("Recording", "Failed to delete audio file");
-//                }
+            if (spokenText.contains("đèn")) {
+                command = command.concat("D");
+            } else if (spokenText.contains("quạt")) {
+                command = command.concat("Q");
             } else {
-                Log.e("Recording", "Audio file does not exist");
+                binding.txtTryAgain.setVisibility(View.VISIBLE);
             }
-        } else {
-            Log.e("Recording", "File name is null");
+            if (spokenText.contains("phòng khách")) {
+                command = command.concat("PK");
+            } else if (spokenText.contains("phòng ngủ")) {
+                command = command.concat("PN");
+            } else {
+                binding.txtTryAgain.setVisibility(View.VISIBLE);
+            }
+            Log.d("WebSocket", "mWebSocketClient: "+  mWebSocketClient + " command "+ command);
+            if (mWebSocketClient != null) {
+                mWebSocketClient.send(command);
+            }
+            else {
+                Toast.makeText(getActivity(), "Không thể kết nối đến server", Toast.LENGTH_SHORT).show();
+            }
         }
     }
+
     @Override
-    public void onDestroy() {
-        super.onDestroy();
-        if (player != null) {
-            player.release();
-            player = null;
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        switch (requestCode) {
+            case REQUEST_RECORD_AUDIO_PERMISSION:
+                permissionToRecordAccepted = grantResults[0] == PackageManager.PERMISSION_GRANTED;
+                break;
         }
+        if (!permissionToRecordAccepted) getActivity().finish();
     }
 }
